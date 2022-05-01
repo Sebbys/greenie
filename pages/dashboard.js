1f775da486df0248911ca453730e84be3ddeccd5
fahri-r <00.fahri.r@gmail.com>
2022-05-01 12:18:19 +0700

    update diagnose views

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -44,7 +44,7 @@ const Dashboard = () => {
             Plant List
           </h2>
 
-          <div class="justify-self-end">
+          <div className="justify-self-end">
             <span className="text-md font-semibold text-gray-700 mr-2">
               Sort:
             </span>
@@ -52,12 +52,12 @@ const Dashboard = () => {
               value={selectedSort}
               onChange={setSelectedSort}
               as="div"
-              class="relative inline-block"
+              className="relative inline-block"
             >
               {/* <!-- Dropdown toggle button --> */}
               {({ open }) => (
                 <>
-                  <Listbox.Button class="flex relative w-32 z-10 block p-2 text-gray-700 bg-white border border-gray-100 rounded-md focus:border-blue-500 focus:ring-opacity-40 focus:ring-blue-300 focus:ring focus:outline-none">
+                  <Listbox.Button className="flex relative w-32 z-10 block p-2 text-gray-700 bg-white border border-gray-100 rounded-md focus:border-blue-500 focus:ring-opacity-40 focus:ring-blue-300 focus:ring focus:outline-none">
                     <p className="grow">{selectedSort.name}</p>
                     {open ? (
                       <Icon
@@ -76,13 +76,13 @@ const Dashboard = () => {
                   {/* <!-- Dropdown menu --> */}
                   <Listbox.Options
                     as="div"
-                    class="absolute right-0 z-20 w-48 py-2 mt-2 bg-white rounded-md shadow-xl"
+                    className="absolute right-0 z-20 w-48 py-2 mt-2 bg-white rounded-md shadow-xl"
                   >
                     {sort.map((s) => (
                       <Listbox.Option
                         key={s.id}
                         value={s}
-                        class="block px-4 py-3 text-sm text-gray-600 capitalize transition-colors duration-200 transform hover:bg-gray-100"
+                        className="block px-4 py-3 text-sm text-gray-600 capitalize transition-colors duration-200 transform hover:bg-gray-100"
                       >
                         {s.name}
                       </Listbox.Option>
--- pages/diagnose.js
+++ pages/diagnose.js
@@ -1,54 +1,193 @@
 import Sidebar from "../components/Sidebar";
+import { Combobox, Transition } from "@headlessui/react";
+import { React, useState, Fragment } from "react";
+import { Icon } from "@iconify/react";
+import useSWR from "swr";
+import qs from "qs";
 
-const Diagnose = () => {
+export const getServerSideProps = async () => {
+  const plantsUrl = `${process.env.URL_API}api/plants?${new URLSearchParams({
+    populate: "*",
+  })}`;
+
+  const [plantsRes] = await Promise.all([fetch(plantsUrl)]);
+  const [plants] = await Promise.all([plantsRes.json()]);
+  return { props: { plants } };
+};
+
+const Diagnose = ({ plants }) => {
+  const [selectedPlant, setSelectedPlant] = useState(plants.data[0]);
+  const [query, setQuery] = useState("");
+  const [checkedSymptoms, setCheckedSymptoms] = useState([]);
+
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
+  const queryUrl = qs.stringify(
+    {
+      populate: ["symptoms", "symptoms.disease", "symptoms.disease.plant"],
+    },
+    {
+      encodeValuesOnly: true,
+    }
+  );
+  const { data: symptoms, error } = useSWR(
+    `${process.env.URL_API}api/plant-parts?${queryUrl}`,
+    fetcher
+  );
+
+  const filteredPlant =
+    query === ""
+      ? plants.data
+      : plants.data.filter((plant) =>
+          plant.attributes.name
+            .toLowerCase()
+            .replace(/\s+/g, "")
+            .includes(query.toLowerCase().replace(/\s+/g, ""))
+        );
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(checkedSymptoms);
+  };
+
+  const handleCheckedSymptoms = (e) => {
+    const isChecked = e.target.checked;
+    if (isChecked) {
+      setCheckedSymptoms([...checkedSymptoms, e.target.value]);
+    } else {
+      const index = checkedSymptoms.indexOf(e.target.value);
+      checkedSymptoms.splice(index, 1);
+      setCheckedSymptoms(checkedSymptoms);
+    }
+  };
+
   return (
     <div className="flex">
       <Sidebar />
       <section className="container p-6 mx-auto bg-white">
         <h2 className="text-xl font-medium text-gray-800 capitalize md:text-2xl">
-          Plant List
+          Diagnose Plant
         </h2>
 
-        <ul class="flex flex-wrap text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:border-gray-700 dark:text-gray-400">
-          <li class="mr-2">
-            <a
-              href="#"
-              aria-current="page"
-              class="inline-block p-4 text-blue-600 bg-gray-100 rounded-t-lg active dark:bg-gray-800 dark:text-blue-500"
-            >
-              Profile
-            </a>
-          </li>
-          <li class="mr-2">
-            <a
-              href="#"
-              class="inline-block p-4 rounded-t-lg hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300"
-            >
-              Dashboard
-            </a>
-          </li>
-          <li class="mr-2">
-            <a
-              href="#"
-              class="inline-block p-4 rounded-t-lg hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300"
-            >
-              Settings
-            </a>
-          </li>
-          <li class="mr-2">
-            <a
-              href="#"
-              class="inline-block p-4 rounded-t-lg hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300"
-            >
-              Contacts
-            </a>
-          </li>
-          <li>
-            <a class="inline-block p-4 text-gray-400 rounded-t-lg cursor-not-allowed dark:text-gray-500">
-              Disabled
-            </a>
-          </li>
-        </ul>
+        <Combobox value={selectedPlant} onChange={setSelectedPlant}>
+          {({ open }) => (
+            <div className="relative mt-5">
+              <div className="relative w-full cursor-default overflow-hidden rounded-md bg-white text-left sm:text-sm">
+                <Combobox.Input
+                  className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md focus:border-blue-400 focus:outline-none"
+                  displayValue={(plant) => plant.attributes.name}
+                  onChange={(event) => setQuery(event.target.value)}
+                />
+                <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-4">
+                  {open ? (
+                    <Icon
+                      className="self-center ml-1 mt-1"
+                      icon="dashicons:arrow-up-alt2"
+                      size="20"
+                    />
+                  ) : (
+                    <Icon
+                      className="self-center ml-1 mt-1"
+                      icon="dashicons:arrow-down-alt2"
+                      size="20"
+                    />
+                  )}
+                </Combobox.Button>
+              </div>
+              <Transition
+                as={Fragment}
+                leave="transition ease-in duration-100"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
+                afterLeave={() => setQuery("")}
+              >
+                <Combobox.Options className="focus:outline-none absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 sm:text-sm">
+                  {filteredPlant.length === 0 && query !== "" ? (
+                    <div className="relative cursor-default select-none py-2 px-4 text-gray-700">
+                      Nothing found.
+                    </div>
+                  ) : (
+                    filteredPlant.map((plant) => (
+                      <Combobox.Option
+                        key={plant.id}
+                        className={({ active }) =>
+                          `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                            active ? "bg-green-500 text-white" : "text-gray-900"
+                          }`
+                        }
+                        value={plant}
+                      >
+                        {({ selected, active }) => (
+                          <>
+                            <span
+                              className={`block truncate ${
+                                selected ? "font-medium" : "font-normal"
+                              }`}
+                            >
+                              {plant.attributes.name}
+                            </span>
+                            {selected ? (
+                              <span
+                                className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                                  active ? "text-white" : "text-teal-600"
+                                }`}
+                              >
+                                <Icon
+                                  className="self-center ml-1 mt-1"
+                                  icon="bi:check-lg"
+                                  size="20"
+                                />
+                              </span>
+                            ) : null}
+                          </>
+                        )}
+                      </Combobox.Option>
+                    ))
+                  )}
+                </Combobox.Options>
+              </Transition>
+            </div>
+          )}
+        </Combobox>
+
+        <form onSubmit={handleSubmit}>
+          {symptoms &&
+            symptoms.data.map((plantPart) => (
+              <div key={plantPart.id} className="mt-10">
+                <div className="bg-white border border-gray-200">
+                  <h2 className="w-full p-4 text-white text-left font-semibold bg-green-600">
+                    {plantPart.attributes.name}
+                  </h2>
+                  <div className="p-4">
+                    {plantPart.attributes.symptoms.data.map((symptom) =>
+                      symptom.attributes.disease.data.attributes.plant.data
+                        .id == selectedPlant.id ? (
+                        <label
+                          key={symptom.id}
+                          className="flex items-center space-x-3 mb-3"
+                        >
+                          <input
+                            type="checkbox"
+                            value={symptom.id}
+                            onChange={handleCheckedSymptoms}
+                            className="appearance-none bg-white h-5 w-5 border border-gray-300 rounded-md checked:bg-green-500 checked:border-transparent focus:outline-none"
+                          />
+                          <span className="text-gray-700 font-normal">
+                            {symptom.attributes.description}
+                          </span>
+                        </label>
+                      ) : null
+                    )}
+                  </div>
+                </div>
+              </div>
+            ))}
+
+          <div className="flex justify-end mt-6">
+            <button className="px-6 py-2 leading-5 text-white transform bg-green-600 rounded-md hover:bg-green-500">
+              Diagnose
+            </button>
+          </div>
+        </form>
       </section>
     </div>
   );
